f3c88d42cd9d5f2efcf3a4308388ddc251a5a82d
mobinjavari <mobinjvr@gmail.com>
2022-02-01 22:20:28 +0330

    create projects & README file & ...

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+function ShowComments()
+{
+    window.document.getElementById('comments').style.display = "inline";
+    
+}
+
+function OpenLink(url)
+{
+    location.href = 'https://'+url;
+}
