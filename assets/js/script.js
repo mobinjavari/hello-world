4fc6680a78cb6b6776b0fbf5d353828815c3c44d
mobinjavari <mobinjvr@gmail.com>
2022-02-02 19:58:26 +0330

    Update Project (responsive, style, html, js)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,12 @@
 
 function ShowComments()
 {
-    window.document.getElementById('comments').style.display = "inline";
-    
+    if(window.document.getElementById('comments').style.display == "none"){
+        window.document.getElementById('comments').style.display = "inline";
+    }
+    else if(window.document.getElementById('comments').style.display == "inline"){
+        window.document.getElementById('comments').style.display = "none";
+    }    
 }
 
 function OpenLink(url)
